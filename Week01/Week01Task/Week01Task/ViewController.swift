ff1a5d88627ae11581feb2724f7a5950d268c9da
altmurzilka <altmurzilka@gmail.com>
2020-05-30 16:06:15 +0600

    Initial commit

---
+++ Week01/Week01Task/Week01Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week01Task
+//
+//  Created by алтынпончик on 5/30/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
