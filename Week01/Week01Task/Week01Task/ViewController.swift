057bb2baf2fcca2a89f1ea1d9aee840a166009fa
altmurzilka <altmurzilka@gmail.com>
2020-05-31 23:28:41 +0600

    basic ui done | no logic added

--- Week01/Week01Task/Week01Task/ViewController.swift
+++ Week01/Week01Task/Week01Task/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func showAlert() {
+        
+        let alert = UIAlertController(title: "Name your color", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        
+        let action = UIAlertAction(title: "Submit", style: .default) { [unowned alert] _ in
+            let answer = alert.textFields![0]
+            // do something interesting with "answer" here
+        }
+        alert.addAction(action)
+
+        present(alert, animated: true)
+    }
 
 
 }
