320943f4ae592d1b7ae72513eaa1f3a02a981a21
altmurzilka <altmurzilka@gmail.com>
2020-06-26 05:47:53 +0600

    ugly

--- Week05/Birdie_MyAttempt/Birdie_MyAttempt/ContentView.swift
+++ Week05/Birdie_MyAttempt/Birdie_MyAttempt/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAddHabbit = false
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            HStack {
+                Text("TEST")
+            }
+            .navigationBarTitle("Home", displayMode: .inline)
+            .navigationBarItems(leading:
+                HStack {
+                    Button(action: { self.showingAddHabbit = true }) {
+                        VStack(alignment: .leading, spacing: 0.0) {
+                            Image("mascot_swift-badge").resizable()
+                                .frame(width: 50, height: 50)
+                            Text("Create Text Post")
+                        }.padding(.top, 20)
+                    }
+                }, trailing:
+                HStack {
+                    Button(action: { self.showingAddHabbit = true }) {
+                        VStack {
+                            Text("Create Image Post").padding(.top, 70)
+                        }
+                    }
+                }
+            )
+        }
     }
 }
 
